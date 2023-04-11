69738240accf752af702f7ab7c84dc33c6eff171
Dennis Creevey <95920143+HasanGocer@users.noreply.github.com>
2023-04-11 23:20:24 +0300

    v 0.66

---
+++ Assets/Scripts/AutoPerSystem.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class AutoPerSystem : MonoBehaviour
+{
+    [SerializeField] Button _perButton;
+    [SerializeField] TMP_Text 
+
+}
